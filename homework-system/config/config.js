324330fc4576861c84a01a0ac94614091eb5e801
CMCgithub <3350322722@qq.com>
2023-09-12 14:57:13 +0800

    delete config.js

--- homework-system/config/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    secret: "bezkoder-secret-key",
-    mongoDB:"mongodb+srv://cmc:m8sE8xqaGrcqt57X@homework-systemdb.kdjzkpx.mongodb.net/?retryWrites=true&w=majority",
-    pass:"VSCPIWTFRHWKUAMK",  
-  };
-  
